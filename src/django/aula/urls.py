e6f23b60753afbc40a0c12dc53ccd154b6a6f383
Cristian Ruppel <licenciadocristianruppel@gmail.com>
2025-11-14 14:47:39 -0300

    Class 04 - Urls Params

--- src/django/aula/urls.py
+++ src/django/aula/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import hello 
+from . import views
 
 urlpatterns = [
-    path('hello/', hello),
+    path('hello/', views.hello),
+    path('bye/', views.bye),
+    path('edad/<int:anios>/<int:futuro>/', views.edad),
 ]
--- src/django/aula/views.py
+++ src/django/aula/views.py
@@ -1,4 +1,14 @@
 from django.http import HttpResponse
+from datetime import date
 
 def hello(request):
     return HttpResponse("Hola mundo desde mi primera vista en Django!")
+
+def bye(request):
+    return HttpResponse("Hasta luego")
+
+def edad(request, anios, futuro):
+    incremento = futuro - date.today().year
+    cumplira = anios + incremento
+    mensaje = "En el año %d cumplirás %d años"%(futuro, cumplira)
+    return HttpResponse(mensaje)
