f1fba118ac4f83fb27baf47ecaad837266e11dbc
Cristian Ruppel <licenciadocristianruppel@gmail.com>
2025-11-15 21:49:48 -0300

    clase 9 admin panel configurado

--- src/django/aula/admin.py
+++ src/django/aula/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Alumno 
+from .models import Alumno,  Persona, Musico, Album
 
 admin.site.register(Alumno)
+admin.site.register(Persona)
+admin.site.register(Musico)
+admin.site.register(Album)
