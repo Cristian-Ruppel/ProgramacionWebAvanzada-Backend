b1b7ed2621e4576e96367dadd784efc091b7930f
Cristian Ruppel <licenciadocristianruppel@gmail.com>
2025-11-16 20:37:13 -0300

    clase 10 postgres y automatizacion

---
+++ runserver.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Le damos 5 segundos a Postgres para que termine de arrancar
+echo "--- Esperando a que Postgres se levante (5 segundos)... ---"
+sleep 5
+
+# 1. Si no existe manage.py, crea el proyecto
+if [ ! -f manage.py ]; then
+  django-admin startproject app .
+fi
+
+# 2. Aplica las migraciones (esto sí es clave)
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+
+# 3. Crea el superusuario SÓLO SI NO EXISTE
+if [ ! -z "$DJANGO_SUPERUSER_NAME" ]; then
+  echo "from django.contrib.auth.models import User; \
+  User.objects.filter(username='$DJANGO_SUPERUSER_NAME').exists() \
+  or User.objects.create_superuser('$DJANGO_SUPERUSER_NAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')" | python manage.py shell
+fi
+
+# 4. Arranca el servidor
+python manage.py runserver 0.0.0.0:8000
